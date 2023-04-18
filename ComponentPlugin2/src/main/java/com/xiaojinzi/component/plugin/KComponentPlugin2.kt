c94523065e5ebd8375e703a02f1fb71cc727b694
xiaojiniz <347837667@qq.com>
2023-04-18 15:16:15 +0800

    feat: 增加一个 plugin2 模块来实现 gradle8.0 的升级

---
+++ ComponentPlugin2/src/main/java/com/xiaojinzi/component/plugin/KComponentPlugin2.kt
@@ -0,0 +1,12 @@
+package com.xiaojinzi.component.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class KComponentPlugin2 : Plugin<Project> {
+
+    override fun apply(target: Project) {
+        println("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx-------------")
+    }
+
+}

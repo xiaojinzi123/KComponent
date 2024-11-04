ad7b58563d1dc62b5492a38d818a3fe4c3490195
xiaojiniz <347837667@qq.com>
2024-11-05 00:17:18 +0800

    feat: 准备对 RouterCenter 进行测试

---
+++ ComponentImpl/src/test/java/com/xiaojinzi/component/RouterCenterTest.kt
@@ -0,0 +1,14 @@
+package com.xiaojinzi.component
+
+import org.junit.Test
+
+/**
+ * [com.xiaojinzi.component.impl.RouterCenter]
+ */
+class RouterCenterTest {
+
+    @Test
+    fun test() {
+    }
+
+}
